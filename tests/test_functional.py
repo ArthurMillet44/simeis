879245f05eb0068dfab18f92fbc9d0f1e2056100
E207838L <milletarthur4@gmail.com>
2026-06-18 09:37:59 +0200

    fix: reformatage des tests pour passer les check de black

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -2,6 +2,7 @@ import requests
 
 BASE_URL = "http://localhost:8080"
 
+
 def test_player_buy_spaceship():
     response = requests.post(f"{BASE_URL}/player/new/toto")
     assert response.status_code == 200
@@ -16,9 +17,15 @@ def test_player_buy_spaceship():
     stations = allPlayer.json()[f"{playerId}"]["stations"]
     totoStationId = next(iter(stations))
 
-    ships = requests.get(f"{BASE_URL}/station/{totoStationId}/shipyard/list", headers={"Simeis-Key": playerKey}).json()["ships"]
+    ships = requests.get(
+        f"{BASE_URL}/station/{totoStationId}/shipyard/list",
+        headers={"Simeis-Key": playerKey},
+    ).json()["ships"]
     firstShipId = ships[0]["id"]
-    buySpaceshipResponse = requests.post(f"{BASE_URL}/station/{totoStationId}/shipyard/buy/{firstShipId}", headers={"Simeis-Key": playerKey})
+    buySpaceshipResponse = requests.post(
+        f"{BASE_URL}/station/{totoStationId}/shipyard/buy/{firstShipId}",
+        headers={"Simeis-Key": playerKey},
+    )
     assert buySpaceshipResponse.status_code == 200
 
     allPlayer = requests.get(f"{BASE_URL}/gamestats")
@@ -36,18 +43,29 @@ def test_player_buy_ship_upgrade():
     stations = allPlayer.json()[f"{playerId}"]["stations"]
     titiStationId = next(iter(stations))
 
-    ships = requests.get(f"{BASE_URL}/station/{titiStationId}/shipyard/list", headers={"Simeis-Key": playerKey}).json()["ships"]
+    ships = requests.get(
+        f"{BASE_URL}/station/{titiStationId}/shipyard/list",
+        headers={"Simeis-Key": playerKey},
+    ).json()["ships"]
     firstShipId = ships[0]["id"]
-    buySpaceshipResponse = requests.post(f"{BASE_URL}/station/{titiStationId}/shipyard/buy/{firstShipId}", headers={"Simeis-Key": playerKey})
+    buySpaceshipResponse = requests.post(
+        f"{BASE_URL}/station/{titiStationId}/shipyard/buy/{firstShipId}",
+        headers={"Simeis-Key": playerKey},
+    )
     assert buySpaceshipResponse.status_code == 200
 
     allPlayer = requests.get(f"{BASE_URL}/gamestats")
     titiMoneyBefore = allPlayer.json()[f"{playerId}"]["money"]
 
-    shipBefore = requests.get(f"{BASE_URL}/ship/{firstShipId}", headers={"Simeis-Key": playerKey}).json()
+    shipBefore = requests.get(
+        f"{BASE_URL}/ship/{firstShipId}", headers={"Simeis-Key": playerKey}
+    ).json()
     cargoCapacityBefore = shipBefore["cargo"]["capacity"]
 
-    buyUpgradeResponse = requests.post(f"{BASE_URL}/station/{titiStationId}/shipyard/upgrade/{firstShipId}/CargoExpansion", headers={"Simeis-Key": playerKey})
+    buyUpgradeResponse = requests.post(
+        f"{BASE_URL}/station/{titiStationId}/shipyard/upgrade/{firstShipId}/CargoExpansion",
+        headers={"Simeis-Key": playerKey},
+    )
     assert buyUpgradeResponse.status_code == 200
     assert buyUpgradeResponse.json()["cost"] == 2400.0
 
@@ -55,7 +73,9 @@ def test_player_buy_ship_upgrade():
     titiMoneyAfter = allPlayer.json()[f"{playerId}"]["money"]
     assert titiMoneyAfter == titiMoneyBefore - 2400.0
 
-    shipAfter = requests.get(f"{BASE_URL}/ship/{firstShipId}", headers={"Simeis-Key": playerKey}).json()
+    shipAfter = requests.get(
+        f"{BASE_URL}/ship/{firstShipId}", headers={"Simeis-Key": playerKey}
+    ).json()
     cargoCapacityAfter = shipAfter["cargo"]["capacity"]
     assert cargoCapacityAfter == cargoCapacityBefore + 120.0
 
@@ -73,11 +93,21 @@ def test_player_buy_heavy_ship_not_enough_money():
     stations = allPlayer.json()[f"{playerId}"]["stations"]
     tataStationId = next(iter(stations))
 
-    ships = requests.get(f"{BASE_URL}/station/{tataStationId}/shipyard/list", headers={"Simeis-Key": playerKey}).json()["ships"]
-    heavyShip = next(ship for ship in ships if ship["reactor_power"] == 10 and ship["cargo_capacity"] == 1200.0)
+    ships = requests.get(
+        f"{BASE_URL}/station/{tataStationId}/shipyard/list",
+        headers={"Simeis-Key": playerKey},
+    ).json()["ships"]
+    heavyShip = next(
+        ship
+        for ship in ships
+        if ship["reactor_power"] == 10 and ship["cargo_capacity"] == 1200.0
+    )
     assert heavyShip["price"] > tataMoneyBefore
 
-    buySpaceshipResponse = requests.post(f"{BASE_URL}/station/{tataStationId}/shipyard/buy/{heavyShip['id']}", headers={"Simeis-Key": playerKey})
+    buySpaceshipResponse = requests.post(
+        f"{BASE_URL}/station/{tataStationId}/shipyard/buy/{heavyShip['id']}",
+        headers={"Simeis-Key": playerKey},
+    )
     assert buySpaceshipResponse.status_code == 200
     assert buySpaceshipResponse.json()["type"].startswith("NotEnoughMoney")
 
@@ -85,5 +115,7 @@ def test_player_buy_heavy_ship_not_enough_money():
     tataMoneyAfter = allPlayer.json()[f"{playerId}"]["money"]
     assert tataMoneyAfter == tataMoneyBefore
 
-    tataPlayer = requests.get(f"{BASE_URL}/player/{playerId}", headers={"Simeis-Key": playerKey}).json()
+    tataPlayer = requests.get(
+        f"{BASE_URL}/player/{playerId}", headers={"Simeis-Key": playerKey}
+    ).json()
     assert tataPlayer["ships"] == []
